5a1deaa2a5bb6389bf1403bd2e6ae92326db8a40
Alexisreau97 <alexis.reaupro@gmail.com>
2018-10-16 13:51:08 +0200

    lancement du jeu

--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -12,29 +12,69 @@ end
 
 class Game
   def initialize
-    #TO DO : créé 2 joueurs, créé un board
-	puts "Joueur 1 écrit ton nom"
-	@player1 = Player.new(gets.chomp, "X")
-	puts "#{player1.name} Tu joue la #{player1.player_value}" 
-	puts "Joueur 2 écrit ton nom"
-	@player2 = Player.new(gets.chomp, "O")
-	puts "#{player2.name} Tu joue le #{player2.player_value}"
-
-
+  #TO DO : créé 2 joueurs, créé un board
+  @array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   end
 
   def go
     # TO DO : lance la partie
+    puts "Joueur 1 écrit ton nom"
+    @player1 = Player.new(gets.chomp, "X")
+    puts "#{@player1.name} Tu joue la #{@player1.player_value}" 
+    puts "Joueur 2 écrit ton nom"
+    @player2 = Player.new(gets.chomp, "O")
+    puts "#{@player2.name} Tu joue le #{@player2.player_value}"
+    
   end
 
   def turn
-    #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
-  end
+  #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
+    i = 0
 
+    if @array[1] == "X" && @array[2] == "X"
+      return puts "win"
+    end
+
+    puts "tour du joueur 1"
+
+    i = gets.chomp
+    @array[i.to_i] = "X"
+
+    puts top_top = "     |     |     "
+    puts top_middle = "  #{@array[1]}  |  #{@array[2]}  |  #{@array[3]}  "
+    puts top_bottom = "_____|_____|_____"
+    puts middle_top = "     |     |     "
+    puts middle_middle = "  #{@array[4]}  |  #{@array[5]}  |  #{@array[6]}  "
+    puts middle_bottom =  "_____|_____|_____"
+    puts bottom_top = "     |     |     "
+    puts bottom_middle = "  #{@array[7]}  |  #{@array[8]}  |  #{@array[9]}  "
+    puts bottom_bottom = "     |     |     "
+
+    if @array[1] == "X" && @array[2] == "X"  
+      return puts "win"
+    end
+
+
+    i = 0
+    puts "Tour du joueur 2"
+    i = gets.chomp
+    @array[i.to_i] = "O"
+
+    puts top_top = "     |     |     "
+    puts top_middle = "  #{@array[1]}  |  #{@array[2]}  |  #{@array[3]}  "
+    puts top_bottom = "_____|_____|_____"
+    puts middle_top = "     |     |     "
+    puts middle_middle = "  #{@array[4]}  |  #{@array[5]}  |  #{@array[6]}  "
+    puts middle_bottom =  "_____|_____|_____"
+    puts bottom_top = "     |     |     "
+    puts bottom_middle = "  #{@array[7]}  |  #{@array[8]}  |  #{@array[9]}  "
+    puts bottom_bottom = "     |     |     "
+
+    turn
+  end
 end
 
 
-array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-puts 
-
-array[2] = @player1.player_value
+Game.new.go
+Board.new
+Game.new.turn
--- tableau.rb
+++ tableau.rb
@@ -9,20 +9,54 @@ puts "  7  |  8  |  9  "
 puts "     |     |     "
 
 
+@array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def trouduc
 i = 0
-puts "Choisi une case"
+
+if @array[1] == "X" && @array[2] == "X"	 
+	return puts "win"
+end
+
+puts "tour du joueur 1"
+
 i = gets.chomp
-array[i.to_i] = "X"
+@array[i.to_i] = "X"
 
-puts "     |     |     "
-puts "  #{array[1]}  |  #{array[2]}  |  #{array[3]}  "
-puts "_____|_____|_____"
-puts "     |     |     "
-puts "  #{array[4]}  |  #{array[5]}  |  #{array[6]}  "
-puts "_____|_____|_____"
-puts "     |     |     "
-puts "  #{array[7]}  |  #{array[8]}  |  #{array[9]}  "
-puts "     |     |     "
+puts top_top = "     |     |     "
+puts top_middle = "  #{@array[1]}  |  #{@array[2]}  |  #{@array[3]}  "
+puts top_bottom = "_____|_____|_____"
+puts middle_top = "     |     |     "
+puts middle_middle = "  #{@array[4]}  |  #{@array[5]}  |  #{@array[6]}  "
+puts middle_bottom =  "_____|_____|_____"
+puts bottom_top = "     |     |     "
+puts bottom_middle = "  #{@array[7]}  |  #{@array[8]}  |  #{@array[9]}  "
+puts bottom_bottom = "     |     |     "
 
+if @array[1] == "X" && @array[2] == "X"	 
+	return puts "win"
+end
+
+
+i = 0
+puts "Tour du joueur 2"
+i = gets.chomp
+@array[i.to_i] = "O"
+
+puts top_top = "     |     |     "
+puts top_middle = "  #{@array[1]}  |  #{@array[2]}  |  #{@array[3]}  "
+puts top_bottom = "_____|_____|_____"
+puts middle_top = "     |     |     "
+puts middle_middle = "  #{@array[4]}  |  #{@array[5]}  |  #{@array[6]}  "
+puts middle_bottom =  "_____|_____|_____"
+puts bottom_top = "     |     |     "
+puts bottom_middle = "  #{@array[7]}  |  #{@array[8]}  |  #{@array[9]}  "
+puts bottom_bottom = "     |     |     "
+
+
+trouduc
+
+end
+
+trouduc
